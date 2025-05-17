e525d971c83b8598367f2e622de51178040c1b7e
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-05-18 08:27:08 +0900

    chore : swagger description 변경

--- src/main/java/neordinary/backend/nteam/controller/DietController.java
+++ src/main/java/neordinary/backend/nteam/controller/DietController.java
@@ -31,7 +31,7 @@ import java.util.UUID;
 public class DietController {
     private final DietService dietService;
 
-    @Operation(summary = "비건 식단 생성", description = "비건 식단을 생성합니다. LLM이 자동으로 처리합니다.")
+    @Operation(summary = "비건 식단 생성", description = "비건 식단을 생성합니다. OpenAI API를 통해 해당 사용자의 알러지, 비건 레벨과 영양 정보 등을 전달하고, 적절한 비건 식단을 생성합니다. 해당 식단은 5일치 생성되며 DB에 영속됩니다.")
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "비건 식단 생성 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청")
@@ -44,7 +44,7 @@ public class DietController {
         return ApiResponse.onSuccess(memberId);
     }
 
-    @Operation(summary = "날짜 기준 비건 식단 조회", description = "지정된 날짜의 모든 비건 식단을 조회합니다. 이미지는 Base64 인코딩 형식입니다. 아침, 점심, 저녁의 식단 정보를 반환합니다.")
+    @Operation(summary = "날짜 기준 비건 식단 조회", description = "지정된 날짜의 모든 비건 식단을 조회합니다. 이미지는 Base64 인코딩 형식입니다. 해당 날짜의 아침, 점심, 저녁의 식단 정보를 반환합니다. 처음 조회되는 식단의 경우 구글의 이미지 API를 통해 이미지를 불러오고, DB에 URL을 저장합니다.")
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "비건 식단 조회 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청")
@@ -58,7 +58,7 @@ public class DietController {
         return ApiResponse.onSuccess(responseDtos);
     }
 
-    @Operation(summary = "비건 식단 상세 조회", description = "지정된 식단의 상세 정보를 반환합니다.")
+    @Operation(summary = "비건 식단 상세 조회", description = "지정된 식단의 상세 정보를 반환합니다. 처음 조회되는 식단의 경우 OpenAI의 API를 통해 사용자와 식단에 맞는 레시피를 생성합니다. 생성된 레시피는 DB에 업데이트 됩니다.")
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "비건 식단 조회 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청")
