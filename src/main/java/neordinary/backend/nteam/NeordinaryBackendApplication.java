27bb6688cd614143cdca15137259f658fce9a725
방진혁 <jinhyeokfang@gmail.com>
2025-05-17 15:12:48 +0900

    initial commit

---
+++ src/main/java/neordinary/backend/nteam/NeordinaryBackendApplication.java
@@ -0,0 +1,13 @@
+package neordinary.backend.nteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeordinaryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeordinaryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neordinary/backend/nteam/NeordinaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package neordinary.backend.nteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeordinaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
