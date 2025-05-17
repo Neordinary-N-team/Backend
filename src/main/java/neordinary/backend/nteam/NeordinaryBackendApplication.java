8ee1d75cb0b731ad6286a2f91d238f115bf61739
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-05-18 05:51:19 +0900

    fix : delete security config

--- src/main/java/neordinary/backend/nteam/NeordinaryBackendApplication.java
+++ src/main/java/neordinary/backend/nteam/NeordinaryBackendApplication.java
@@ -1,10 +1,13 @@
 package neordinary.backend.nteam;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://neohack.duckdns.org", description = "도메인 설명")})
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
