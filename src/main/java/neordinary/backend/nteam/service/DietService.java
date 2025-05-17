dbd61760949f50389accfb3901b0a143a39500e6
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-05-18 02:50:08 +0900

    feat : 자동 식단 갱신 로직 추가

--- src/main/java/neordinary/backend/nteam/service/DietService.java
+++ src/main/java/neordinary/backend/nteam/service/DietService.java
@@ -11,6 +11,7 @@ import neordinary.backend.nteam.gpt.GPTApiClient;
 import neordinary.backend.nteam.gpt.openai.meal_plan.GPTResponseMealPlanDto;
 import neordinary.backend.nteam.repository.DietRepository;
 import neordinary.backend.nteam.repository.MemberRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -65,6 +66,21 @@ public class DietService {
         dietRepository.saveAll(diets);
 
         return memberId;
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * MON") // 매주 일요일 0시 실행
+    public void createDietScheduled() {
+        // TODO : 멀티 스레드 리팩토링 필요
+        List<Member> members = memberRepository.findAll();
+
+        for (Member member : members) {
+            List<GPTResponseMealPlanDto> gptResponseMealPlanDtos = gptApiClient.generateMealPlan(member);
+            List<Diet> diets = gptResponseMealPlanDtos.stream()
+                    .map(GPTResponseMealPlanDto::toEntity)
+                    .toList();
+            dietRepository.saveAll(diets);
+        }
     }
 
     // TODO : get diets 하면 사진 없을 때 사진 생성 필요
